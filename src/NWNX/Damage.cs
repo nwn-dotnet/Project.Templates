aa096d924f69f81b51118382506e469097bb40d6
GideonCrawle <crawle.gideon@gmail.com>
2020-03-16 04:48:48 +0000

    Added iRanged parameter to NWNX_Damage_DealDamage() and added new function NWNX_Area_GetTileModelResRef()

--- src/NWNX/Damage.cs
+++ src/NWNX/Damage.cs
@@ -123,9 +123,10 @@ namespace NWN.NWNX
         }
 
         // Deal damage to target - permits multiple damage types and checks enhancement bonus for overcoming DR
-        public static void DealDamage(DamageEventData data, NWObject oTarget, NWObject oSource)
+        public static void DealDamage(DamageEventData data, NWObject oTarget, NWObject oSource, bool iRanged = false)
         {
-            Internal.NativeFunctions.nwnxSetFunction(PLUGIN_NAME, "SetAttackEventData");
+            Internal.NativeFunctions.nwnxSetFunction(PLUGIN_NAME, "DealDamage");
+            Internal.NativeFunctions.nwnxPushInt(iRanged ? 1 : 0);
             Internal.NativeFunctions.nwnxPushInt(data.Base);
             Internal.NativeFunctions.nwnxPushInt(data.Sonic);
             Internal.NativeFunctions.nwnxPushInt(data.Positive);
