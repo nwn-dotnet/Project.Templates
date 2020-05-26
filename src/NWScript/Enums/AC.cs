9b6f4b276ac0bdab4e2d7eaa740b31ec38535b64
Zunath <coolty3001@yahoo.com>
2020-05-26 18:22:20 -0400

    Create new AC enum and fix the ACIncrease / ACDecrease functions to use those instead of the DamageType. The comment from NWScript is misleading - these don't use DamageTypes, they use a different set of values for it.

---
+++ src/NWScript/Enums/AC.cs
@@ -0,0 +1,10 @@
+﻿namespace NWN.Enums {
+    public enum AC {
+        ArmorEnchantmentBonus = 2,
+        DeflectionBonus = 4,
+        DodgeBonus = 0,
+        NaturalBonus = 1,
+        ShieldEnchantmentBonus = 3,
+        VsDamageTypeAll = 4103
+    }
+}
